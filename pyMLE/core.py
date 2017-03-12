554c255f1345008c4fd49c538cef21af34d33fdf
Ze Vinicius <jvmirca@gmail.com>
2017-03-12 10:53:32 -0700

    implement fisher info matrix using autograd

--- pyMLE/core.py
+++ pyMLE/core.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from autograd import grad
 from scipy.optimize import minimize, curve_fit
+
 
 __all__ = ['MultinomialLikelihood']
 
@@ -9,7 +11,7 @@ class MultinomialLikelihood(object):
     Parameters
     ----------
     data : ndarray
-        Observed data.
+        Observed count data.
     prob_model : callable
         Events probabilities of the multinomial distribution.
     """
@@ -17,6 +19,14 @@ class MultinomialLikelihood(object):
     def __init__(self, data, prob_model):
         self.data = data
         self.prob_model = prob_model
+
+    @property
+    def n_bins(self):
+        return len(self.data)
+
+    @property
+    def n_counts(self):
+        return self.data.sum()
 
     def evaluate(self, params):
         """
@@ -42,11 +52,29 @@ class MultinomialLikelihood(object):
             Dictionary for additional arguments. See scipy.optimize.minimize.
         """
         self.opt_result = minimize(self.evaluate, x0=x0, **kwargs)
-
         return self.opt_result
 
     def fisher_information_matrix(self):
         """
         Computes the Fisher Information Matrix
         """
-        pass
+        n_params = len(self.opt_result.x)
+        fisher = np.empty(shape=(n_params, n_params))
+        grad_prob_model = np.empty(shape=(n_params))
+        opt_params = self.opt_result.x
+
+        for i in range(n_params):
+            grad_prob_model = grad(self.prob_model, argnum=i)
+
+        for i in range(n_params):
+            for j in range(i):
+                fisher[i, j] = (grad_prob_model[i](*opt_params) *
+                                grad_prob_model[j](*opt_params) /
+                                self.prob_model(*opt_params)).sum()
+                fisher[j, i] = fisher[i, j]
+
+        return self.n_counts * fisher
+
+    def uncertainties(self):
+        inv_fisher = np.linalg.inv(self.fisher_information_matrix())
+        return np.sqrt(np.diag(inv_fisher))
