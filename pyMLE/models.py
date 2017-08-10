118e8bdefcf4975fb8b49d1d41a093203df71b27
Ze Vinicius <jvmirca@gmail.com>
2017-08-10 16:52:49 -0700

    add a few more models

--- pyMLE/models.py
+++ pyMLE/models.py
@@ -1,15 +1,35 @@
 import numpy as np
+from scipy.special import erf
+
+class SymmetricGaussian2D(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def evaluate(self, A, xo, yo, s):
+        return A * np.exp(-0.5 * (((self.x - xo) ** 2 + (self.y - yo) ** 2)) / s ** 2)
 
 class Gaussian2D(object):
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.n_params = 6
 
     def evaluate(self, A, xo, yo, a, b, c):
-        return A * np.exp(- (a * (self.x - xo) ** 2
-                             - 2 * b * (self.x - xo) * (self.y - yo)
-                             + c * (self.y - yo) ** 2))
+        return A * np.exp(-(a * (self.x - xo) ** 2
+                            - 2 * b * (self.x - xo) * (self.y - yo)
+                            + c * (self.y - yo) ** 2))
+
+class IntegratedSymmetricGaussian2D(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def evaluate(self, F, xo, yo, s):
+        return (F / 4 *
+                ((erf((self.x - xo + 0.5) / (np.sqrt(2) * s)) -
+                  erf((self.x - xo - 0.5) / (np.sqrt(2) * s))) *
+                 (erf((self.y - yo + 0.5) / (np.sqrt(2) * s)) -
+                  erf((self.y - yo - 0.5) / (np.sqrt(2) * s)))))
 
 class ExpSquaredKernel(object):
     def __init__(self, t):
