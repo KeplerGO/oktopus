ad1a643968b0d06987ba86f301a70bc8a8d743f3
Ze Vinicius <jvmirca@gmail.com>
2017-09-15 15:24:17 -0700

    wraps np.random.multivariate_normal

--- octopus/models.py
+++ octopus/models.py
@@ -6,6 +6,7 @@ import math
 class Kernel(ABC):
     pass
 
+
 class SymmetricGaussian2D(object):
     def __init__(self, x, y):
         self.x = x
@@ -13,6 +14,7 @@ class SymmetricGaussian2D(object):
 
     def evaluate(self, A, xo, yo, s):
         return A * np.exp(-0.5 * (((self.x - xo) ** 2 + (self.y - yo) ** 2)) / s ** 2)
+
 
 class Gaussian2D(object):
     def __init__(self, x, y):
@@ -23,6 +25,7 @@ class Gaussian2D(object):
         return A * np.exp(-(a * (self.x - xo) ** 2
                             - 2 * b * (self.x - xo) * (self.y - yo)
                             + c * (self.y - yo) ** 2))
+
 
 class IntegratedSymmetricGaussian2D(object):
     def __init__(self, x, y):
@@ -36,12 +39,14 @@ class IntegratedSymmetricGaussian2D(object):
                  (erf((self.y - yo + 0.5) / (np.sqrt(2) * s)) -
                   erf((self.y - yo - 0.5) / (np.sqrt(2) * s)))))
 
+
 class ExpSquaredKernel(Kernel):
     def __init__(self, t):
         self.t = t
 
     def evaluate(self, k, l):
         return k * np.exp(- l ** 2 * (self.t[:, None] - self.t[None, :]) ** 2)
+
 
 class ExpSquaredKernel2D(Kernel):
     def __init__(self, t):
@@ -50,12 +55,14 @@ class ExpSquaredKernel2D(Kernel):
     def evaluate(self, k, l):
         return k * np.exp(- l ** 2 * np.sum((self.t[:, None] - self.t[None, :]) ** 2, axis=-1))
 
+
 class WhiteNoiseKernel(Kernel):
     def __init__(self, n):
         self.n = n
 
     def evaluate(self, s):
         return np.diag(np.ones(self.n) * s ** 2)
+
 
 def init_guesses(data):
     """
@@ -75,14 +82,14 @@ def init_guesses(data):
 
     total = data.sum()
     Y, X = np.indices(data.shape)
-    x = (X*data).sum()/total
-    y = (Y*data).sum()/total
+    x = (X * data).sum() / total
+    y = (Y * data).sum() / total
 
     marg_x = data[:, int(x)]
     marg_y = data[int(y), :]
 
-    sigma_y = math.sqrt(np.abs((np.arange(marg_y.size) - y)**2*marg_y).sum()/marg_y.sum())
-    sigma_x = math.sqrt(np.abs((np.arange(marg_x.size) - x)**2*marg_x).sum()/marg_x.sum())
+    sigma_y = math.sqrt(np.abs((np.arange(marg_y.size) - y) ** 2 * marg_y).sum() / marg_y.sum())
+    sigma_x = math.sqrt(np.abs((np.arange(marg_x.size) - x) ** 2 * marg_x).sum() / marg_x.sum())
     sigma = math.sqrt((sigma_x**2 + sigma_y**2)/2.0)
 
     return total, x, y, sigma
