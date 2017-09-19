be7d7d98dc29a9db6266174b3fcea912c57708a5
Ze Vinicius <jvmirca@gmail.com>
2017-09-19 12:22:09 -0700

    more code translation

--- octopus/kepler_prf.py
+++ octopus/kepler_prf.py
@@ -50,13 +50,13 @@ class KeplerPRF(KeplerTargetPixelFile):
     """
     Kepler's Pixel Response Function
 
-    This class provides the necessary interface to load the Kepler PSF
+    This class provides the necessary interface to load Kepler PSF
     calibration files and to create a model that can be fit as a function
     of flux and centroid position.
     """
 
-    def __init__(self, file_dir):
-        self.file_dir = file_dir
+    def __init__(self, prf_files_dir):
+        self.prf_files_dir = prf_files_dir
 
     def prf_to_detector(self, F, xo, yo):
         self.prf_model = np.zeros((np.size(self.y), np.size(self.x)))
@@ -89,78 +89,71 @@ class KeplerPRF(KeplerTargetPixelFile):
     def evaluate(self, F, xo, yo):
         return self.prf_to_detector(F, xo, yo):
 
-    def interpolate(self, X, Y, values):
-        # should return an instance of scipy.interpolate.RectBivariateSpline(X, Y, values)
+    def __call__(self, F, xo, yo):
+        return self.evaluate()
+
 
     def read_prf_calibration_file(self, path, ext):
         prf_cal_file = pyfits.open(path)
         data = prf_cal_file[ext].data
-        crpix1p = prf_cal_file[ext].header['CRPIX1P']
-        crpix2p = prf_cal_file[ext].header['CRPIX2P']
+        #crpix1p = prf_cal_file[ext].header['CRPIX1P']
+        #crpix2p = prf_cal_file[ext].header['CRPIX2P']
+        # looks like these data below are the same for all prf calibration files
         crval1p = prf_cal_file[ext].header['CRVAL1P']
         crval2p = prf_cal_file[ext].header['CRVAL2P']
         cdelt1p = prf_cal_file[ext].header['CDELT1P']
         cdelt2p = prf_cal_file[ext].header['CDELT2P']
         prf_cal_file.close()
-        return data, crpix1p, crpix2p, crval1p, crval2p, cdelt1p, cdelt2p
+        #return data, crpix1p, crpix2p, crval1p, crval2p, cdelt1p, cdelt2p
+        return data, crval1p, crval2p, cdelt1p, cdelt2p
 
     def prepare_prf(self):
+        n_hdu = 5
+        min_prf_weight = 1e-6
+        # determine suitable PRF calibration file
+        if int(module) < 10:
+            prefix = 'kplr0'
+        else:
+            prefix = 'kplr'
+        prf_file_path = os.path.join(self.prf_files_dir,
+                                     prefix + module + '.' + output + '*_prf.fits')
+        prffile = glob.glob(prf_file_path)[0]
 
+        # read PRF images
+        prfn = [0] * n_hdu
+        crval1p = np.zeros(n_hdu, dtype='float32')
+        crval2p = np.zeros(n_hdu, dtype='float32')
+        cdelt1p = np.zeros(n_hdu, dtype='float32')
+        cdelt2p = np.zeros(n_hdu, dtype='float32')
+        for i in range(1, n_hdu+1)::/
+            prfn[i], crval1p[i], crval2p[i], cdelt1p[i], cdelt2p[i] = self.read_prf_calibration_file(prffile, i)
+        prfn = np.array(prfn)
+        PRFx = np.arange(0.5, np.shape(prfn[0])[1] + 0.5)
+        PRFy = np.arange(0.5, np.shape(prfn[0])[0] + 0.5)
+        PRFx = (PRFx - np.size(PRFx) / 2) * cdelt1p[0]
+        PRFy = (PRFy - np.size(PRFy) / 2) * cdelt2p[0]
 
+        # interpolate the calibrated PRF shape to the target position
+        prf = np.zeros(np.shape(prfn[0]), dtype='float32')
+        prfWeight = np.zeros(n_hdu, dtype='float32')
+        ref_column = column + (xdim - 1.) / 2.
+        ref_row = row + (ydim - 1.) / 2.
+        for i in range(n_hdu):
+            prfWeight[i] = math.sqrt((ref_column - crval1p[i]) ** 2
+                                     + (ref_row - crval2p[i]) ** 2))
+            if prfWeight[i] < minimum_prf_weight:
+                prfWeight[i] = minimum_prf_weight
+            prf += prfn[i] / prfWeight[i]
+        prf /= (np.nansum(prf) * cdelt1p[0] * cdelt2p[0])
 
-#def read_and_interpolate_prf(prfdir, module, output, column, row, xdim, ydim,
-#                             verbose=False, logfile='kepprf.log'):
-#    """
-#    Read PRF file and prepare the data to be used for evaluating PRF.
-#
-#    Parameters
-#    ----------
-#    prfdir : str
-#        The full or relative directory path to a folder containing the Kepler
-#        PSF calibration. Calibration files can be downloaded from the Kepler
-#        focal plane characteristics page at the MAST.
-#    module : str
-#        The 'MODULE' keyword from TPF file.
-#    output : str
-#        The 'OUTPUT' keyword from TPF file.
-#    column : int
-#        The '1CRV5P' keyword from TPF[1] file.
-#    row : int
-#        The '2CRV5P' keyword from TPF[1] file.
-#    xdim : int
-#        The first part of the 'TDIM5' keyword from TPF[1] file.
-#    ydim : int
-#        The second part of the 'TDIM5' keyword from TPF[1] file.
-#    verbose : boolean
-#        Print informative messages and warnings to the shell and logfile?
-#    logfile : string
-#        Name of the logfile containing error and warning messages.
-#
-#    Returns
-#    -------
-#    splineInterpolation
-#        You can get PRF at given position:
-#        kepfunc.PRF2DET([flux], [x], [y], DATx, DATy, 1.0, 1.0, 0.0, splineInterpolation)
-#    DATx : numpy.array
-#        X-axis coordiantes of pixels for given TPF
-#    DATy : numpy.array
-#        Y-axis coordinates of pixels for given TPF
-#    prf : numpy.array
-#        PRF interpolated to given position on the camera
-#    PRFx : numpy.array
-#        X-axis coordinates of prf values
-#    PRFy : numpy.array
-#        Y-axis coordinates of prf values
-#    PRFx0 : int
-#    PRFy0 : int
-#    cdelt1p : numpy.array
-#        CDELT1P values from 5 HDUs of PRF file.
-#    cdelt2p : numpy.array
-#        CDELT2P values from 5 HDUs of PRF file.
-#    prfDimX : int
-#        size of PRFx
-#    prfDimY : int
-#        size of PRFy
-#
-#    """
+        # location of the data image centered on the PRF image (in PRF pixel units)
+        prfDimY = int(ydim / cdelt1p[0])
+        prfDimX = int(xdim / cdelt2p[0])
+        PRFy0 = int(np.round((np.shape(prf)[0] - prfDimY) / 2))
+        PRFx0 = int(np.round((np.shape(prf)[1] - prfDimX) / 2))
+        DATx = np.arange(column, column + xdim)
+        DATy = np.arange(row, row + ydim)
+        splineInterpolation = scipy.interpolate.RectBivariateSpline(PRFx, PRFy, prf)
 
+        return (splineInterpolation, DATx, DATy, prf, PRFx, PRFy, PRFx0, PRFy0,
+                cdelt1p, cdelt2p, prfDimX, prfDimY)
