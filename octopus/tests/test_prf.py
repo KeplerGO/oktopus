8f89d3f15cc172294ebf57a1568e74e87519a800
Geert Barentsen <geert@barentsen.be>
2017-09-23 14:18:59 -0700

    Make the tests more comprehensive

--- octopus/tests/test_prf.py
+++ octopus/tests/test_prf.py
@@ -7,33 +7,34 @@ from .. import models
 from .. import core
 from ..kepler_prf import KeplerPRF
 
-PRFDIR = os.path.expanduser('~/.pyke/kepler-prf-calibration-data/')
 
-
-def test_prf_integration():
-    """Does the PRF model integrate to the requested flux?"""
-    channel, col, row = 84, 200, 800
-    shape = (18, 14)
-    flux = 100
-    prf = KeplerPRF(prf_files_dir=PRFDIR, channel=channel, column=col, row=row, shape=shape)
-    prf_sum = prf.evaluate(flux, col + shape[0]/2, row + shape[1]/2, 0).sum()
-    assert np.isclose(prf_sum, flux, rtol=0.1)
+def test_prf_normalization():
+    """Does the PRF model integrate to the requested flux across the focal plane?"""
+    for channel in [1, 20, 40, 60, 84]:
+        for col in [123, 678]:
+            for row in [234, 789]:
+                shape = (18, 14)
+                flux = 100
+                prf = KeplerPRF(channel=channel, column=col, row=row, shape=shape)
+                prf_sum = prf.evaluate(flux, col + shape[0]/2, row + shape[1]/2, 0).sum()
+                assert np.isclose(prf_sum, flux, rtol=0.1)
 
 
 def test_prf_vs_aperture_photometry():
     """Is the PRF photometry result consistent with simple aperture photometry?"""
     tpf = fits.open(os.path.join(PATH_TESTDATA, "ktwo201907706-c01-first-cadence.fits.gz"))
     col, row = 173, 526
-    prf = KeplerPRF(prf_files_dir=PRFDIR, channel=tpf[0].header['CHANNEL'],
+    prf = KeplerPRF(channel=tpf[0].header['CHANNEL'],
                     column=col, row=row,
                     shape=tpf[1].data.shape)
-    fluxo, xo, yo, bkgo = models.get_initial_guesses(data=tpf[1].data,
+    fluxo, colo, rowo, bkgo = models.get_initial_guesses(data=tpf[1].data,
                                                      X=prf.x,
                                                      Y=prf.y)
     aperture_flux = tpf[1].data.sum()
     logL = core.PoissonLikelihood(tpf[1].data, prf.evaluate)
-    fitresult = logL.fit((fluxo, xo, yo, bkgo))
-    prf_flux = fitresult.x[0]
-    prf_bkg = fitresult.x[3]
+    fitresult = logL.fit((fluxo, colo, rowo, bkgo))
+    prf_flux, prf_col, prf_row, prf_bkg = fitresult.x
+    assert np.isclose(prf_col, col, atol=5)
+    assert np.isclose(prf_row, row, atol=5)
     assert np.isclose(prf_bkg, np.percentile(tpf[1].data, 10), rtol=0.1)
     assert np.isclose(aperture_flux, prf_flux, rtol=0.1)
