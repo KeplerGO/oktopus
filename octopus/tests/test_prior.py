8681e8bfb474f5708bfcb1772bef6a8e386a1b33
Ze Vinicius <jvmirca@gmail.com>
2017-09-25 20:54:08 -0700

    api change: priors should be given as a list

--- octopus/tests/test_prior.py
+++ octopus/tests/test_prior.py
@@ -11,28 +11,22 @@ def test_uniform_prior():
     assert np.isfinite(unif(x))
     assert not np.isfinite(unif(x + 1))
 
-def test_add_uniform_priors():
-    unif = UniformPrior(-.5, .5) + UniformPrior(.5, 1.)
-    assert isinstance(unif, UniformPrior)
+def test_joint_uniform_priors():
+    unif = JointPrior(UniformPrior(-.5, .5), UniformPrior(.5, 1.))
     assert np.isfinite(unif((.4999, .5)))
     assert ~np.isfinite(unif((.5, .5)))
-
-    unif = unif + UniformPrior(2., 3.)
-    assert isinstance(unif, UniformPrior)
-    assert np.isfinite(unif((.4999, .5, 2.5)))
-    assert ~np.isfinite(unif((.5, .5, .0)))
 
 def test_gaussian_prior():
     gauss = GaussianPrior(0, 1)
     assert gauss(0) == 0.0
     assert gauss(4) == 8.0
 
-def test_add_gaussian_priors():
-    gauss = GaussianPrior(0, 1) + GaussianPrior(1, 1)
+def test_joint_gaussian_priors():
+    gauss = JointPrior(GaussianPrior(0, 1), GaussianPrior(1, 1))
     assert gauss((0, 1)) == 0.0
     assert gauss((2, 3)) == 4.0
 
-def test_joint_prior():
+def test_joint_mixed_prior():
     unif = UniformPrior(-1, 1)
     gauss = GaussianPrior(0, 1)
     jp = JointPrior(unif, gauss)
@@ -40,6 +34,5 @@ def test_joint_prior():
     assert jp.evaluate((.5, .5)) == unif.evaluate(.5) + gauss.evaluate(.5)
     assert not np.isfinite(jp((1.5, .5)))
 
-    jp = jp + jp
+    jp = JointPrior(unif, unif, gauss, gauss)
     assert jp.evaluate((.5, .5, .5, .5)) == 2 * (unif.evaluate(.5) + gauss.evaluate(.5))
-    assert not np.isfinite(jp((1.5, 1.5, 1.5, 1.5)))
