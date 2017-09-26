aa046ce32dcc014002beb06c87f2d1c359a90ab2
Ze Vinicius <jvmirca@gmail.com>
2017-09-26 11:27:18 -0700

    reload webpage

---
+++ searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:53,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{And:0,all:0,clone:0,com:0,git:0,github:0,http:0,index:0,instal:0,mirca:0,modul:0,page:0,pip:0,run:0,search:0,set:0,you:0},titles:["Welcome to \ud83d\udc19 Oktopus!"],titleterms:{indic:0,oktopu:0,tabl:0,welcom:0}})
