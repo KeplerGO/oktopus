611e9fe0281569ab818effca4c88b416d5252ffd
Ze Vinicius <jvmirca@gmail.com>
2017-09-28 17:15:31 -0700

    add autosummary extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,6 +34,7 @@ import sphinx_rtd_theme
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
