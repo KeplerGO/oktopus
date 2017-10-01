5c81176da7409d5103eddd690f16c9c75580c3a1
Ze Vinicius <jvmirca@gmail.com>
2017-09-30 18:19:13 -0700

    add unit tests for posterior distributions

---
+++ oktopus/tests/test_posterior.py
@@ -0,0 +1,38 @@
+import pytest
+import autograd.numpy as npa
+from math import sqrt
+import numpy as np
+from ..likelihood import PoissonLikelihood
+from ..posterior import Posterior, GaussianPosterior, PoissonPosterior
+from ..prior import UniformPrior, GaussianPrior
+
+
+@pytest.mark.parametrize("toy_data",
+                         ([np.random.randint(1, 20, size=100)],
+                          [np.random.randint(1, 10, size=50)]))
+def test_posterior(toy_data):
+    mean = lambda l: npa.array([l])
+    unif_prior = UniformPrior(lb=np.min(toy_data), ub=np.max(toy_data))
+    logP = PoissonPosterior(data=toy_data, mean=mean, prior=unif_prior)
+    mean_hat = logP.fit(x0=np.median(toy_data))
+    np.testing.assert_almost_equal(mean_hat.x, np.mean(toy_data), decimal=4)
+
+    # test that creating a Posterior object results in the same estimations
+    logP_ = Posterior(likelihood=PoissonLikelihood(data=toy_data, mean=mean),
+                      prior=unif_prior)
+    mean_hat_ = logP_.fit(x0=np.median(toy_data))
+    np.testing.assert_almost_equal(mean_hat.x, mean_hat_.x)
+
+    # Using a Gaussian prior
+    gauss_prior = GaussianPrior(mean=np.mean(toy_data), var=np.mean(toy_data))
+    logP.prior = gauss_prior
+    mean_hat = logP.fit(x0=np.median(toy_data))
+    np.testing.assert_almost_equal(mean_hat.x, np.mean(toy_data), decimal=4)
+
+    # Maybe we can approximate the count data as a Gaussian distribution
+    logP = GaussianPosterior(data=toy_data, mean=mean, var=np.std(toy_data) ** 2, prior=unif_prior)
+    mean_hat = logP.fit(x0=np.median(toy_data))
+    np.testing.assert_almost_equal(mean_hat.x, np.mean(toy_data), decimal=4)
+    logP.prior = gauss_prior
+    mean_hat = logP.fit(x0=np.median(toy_data))
+    np.testing.assert_almost_equal(mean_hat.x, np.mean(toy_data), decimal=4)
