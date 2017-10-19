48051f2cb473aaa0709f045b6f3b73325eabb9b9
Ze Vinicius <jvmirca@gmail.com>
2017-10-19 12:00:56 -0700

    add unit tests for basinhopping

--- oktopus/tests/test_likelihood.py
+++ oktopus/tests/test_likelihood.py
@@ -23,18 +23,22 @@ def test_multinomial_likelihood(counts, p0, ans):
     np.testing.assert_almost_equal(neg_log_jeff_prior, logL.jeffreys_prior(p_hat.x))
 
 
-@pytest.mark.parametrize("toy_data",
-                         ([np.random.randint(1, 20, size=100)],
-                          [np.random.randint(1, 10, size=50)]))
-def test_poisson_likelihood(toy_data):
+@pytest.mark.parametrize("toy_data, optimizer",
+                         ([np.random.randint(1, 20, size=100), 'basinhopping'],
+                          [np.random.randint(1, 20, size=100), 'minimize'],
+                          [np.random.randint(1, 10, size=50), 'basinhopping'],
+                          [np.random.randint(1, 10, size=50), 'minimize']))
+def test_poisson_likelihood(toy_data, optimizer):
     mean = lambda l: npa.array([l])
     logL = PoissonLikelihood(data=toy_data, mean=mean)
-    mean_hat = logL.fit(x0=np.median(toy_data))
+    mean_hat = logL.fit(optimizer=optimizer, x0=np.median(toy_data))
     np.testing.assert_almost_equal(mean_hat.x, np.mean(toy_data), decimal=4)
     np.testing.assert_almost_equal(logL.uncertainties(mean_hat.x),
                                    sqrt(np.mean(toy_data)), decimal=4)
 
-def test_gaussian_likelihood():
+
+@pytest.mark.parametrize("optimizer", (["basinhopping"]))
+def test_gaussian_likelihood(optimizer):
     x = npa.linspace(-5, 5, 20)
     np.random.seed(0)
     fake_data = x * 3 + 10 + np.random.normal(scale=2, size=x.shape)
@@ -42,7 +46,7 @@ def test_gaussian_likelihood():
         return alpha * x + beta
     logL = GaussianLikelihood(fake_data, line, 4)
     p0 = (1, 1) # dumb initial_guess for alpha and beta
-    p_hat = logL.fit(x0=p0)
+    p_hat = logL.fit(optimizer=optimizer, x0=p0)
     # lsq solution from linear algebra
     M = np.array([[np.sum(x * x), np.sum(x)], [np.sum(x), len(x)]])
     p_hat_linalg = np.dot(np.linalg.inv(M),
